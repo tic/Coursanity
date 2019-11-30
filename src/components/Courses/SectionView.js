c27108460deb7701100060fa0f6dd3c1b4bb636c
png <jasona99@outlook.com>
2019-11-30 15:22:16 -0500

    Setting up more section view schtuff.

---
+++ src/components/Courses/SectionView.js
@@ -0,0 +1,16 @@
+
+
+export default class SectionView extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        //info
+        //calendar
+        //maps
+        //grades
+        //rating
+            //coming soon
+    }
+}
