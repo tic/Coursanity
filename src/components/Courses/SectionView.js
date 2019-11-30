709a2e38206bc1031a136ee45e1666848413807c
png <jasona99@outlook.com>
2019-11-30 16:04:09 -0500

    Basis for rest of Section view.

--- src/components/Courses/SectionView.js
+++ src/components/Courses/SectionView.js
@@ -1,4 +1,9 @@
-
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { FontAwesome } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient'
+import Colors from '../../constants/Colors';
 
 export default class SectionView extends React.Component{
     constructor(props){
@@ -12,5 +17,13 @@ export default class SectionView extends React.Component{
         //grades
         //rating
             //coming soon
+        <View>
+            <ScrollView>
+                <InfoBox professors={this.state.professors} meetings={this.state.meetings}/>
+                <MeetingCalendar meetings={this.state.meetings}/>
+                <MeetingLocation meetings={this.state.meetings}/>
+                <GradeBox professors={this.state.professors}/>
+            </ScrollView>
+        </View>
     }
 }
